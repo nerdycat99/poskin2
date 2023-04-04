bcb368de72022a8523875c8bd3bf1ad205c4c8f2
nerdycat99 <info@drysky.co.uk>
2023-04-04 13:47:05 +1000

    seeds and retail price calculation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Default Country codes
+Country.find_or_create_by({ country: "Australia", code: "AUD" })
+Country.find_or_create_by({ country: "New Zealand", code: "NZL" })
+
+# Default Tax Rates for GST
+TaxRate.find_or_create_by({ rate: "10", name: "standard" })
+TaxRate.find_or_create_by({ rate: "0", name: "none" })
+
+# Default Accounting Codes
+AccountingCode.find_or_create_by({ name: "CONS001", enabled: true, description: "Consignment" })
+
+# Default Product Attribute Types
+AccountingCode.find_or_create_by({ name: "size" })
+AccountingCode.find_or_create_by({ name: "colour" })
+AccountingCode.find_or_create_by({ name: "artists_code" })
