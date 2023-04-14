ad4ea5842881b198dda7351533088f968fc27190
nerdycat99 <info@drysky.co.uk>
2023-04-15 09:18:25 +1000

    P022-product-creation-options

--- app/javascript/controllers/product_controller.js
+++ app/javascript/controllers/product_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="product"
 export default class extends Controller {
-    static targets = [
+  static targets = [
     "calculateRetailPriceSection",
     "calculateCostPriceSection",
     "registeredForSalesTax",
@@ -18,7 +18,16 @@ export default class extends Controller {
     "costPrice",
     "percentage",
     "retailPrice",
-    "retailPercentage"
+    "retailPercentage",
+    "retailCalculatedCostPrice",
+    "retailCalculatedCostPriceTax",
+    "retailCalculatedCostPriceTotal",
+    "retailCalculatedRetailPrice",
+    "retailCalculatedRetailPriceTax",
+    "retailCalculatedRetailPriceTotal",
+    "retailCalculatedMarkupAmount",
+    "retailCalculatedProfitAmount",
+    "retailCalculatedRetailTaxLiability"
   ]
   connect() {
   }
@@ -37,7 +46,7 @@ export default class extends Controller {
     }
 
     if (selectedOption == '0') {
-      this.hide(this.calculateCostPriceSectionTarget.innerText)
+      this.hide(this.calculateCostPriceSectionTarget)
       this.hide(this.calculateRetailPriceSectionTarget)
     }
   }
@@ -50,8 +59,54 @@ export default class extends Controller {
   }
 
   retailPriceDataEntered() {
-    // console.log(typeof this.registeredForSalesTaxTarget.innerText === 'string')
-    // console.log(this.registeredForSalesTaxTarget)
+    console.log('in here')
+    var registeredForTax = (this.registeredForSalesTaxTarget.innerText === 'true')
+    if (this.retailPercentageTarget.value && this.retailPriceTarget.value) {
+      this.calculateAmountsForGivenRetailPrice(registeredForTax)
+    }
+  }
+
+  calculateAmountsForGivenRetailPrice(registeredForTax) {
+    var retailPrice = parseFloat(this.retailPriceTarget.value.replace('$','')).toFixed(2)
+    var percentage = parseFloat(this.retailPercentageTarget.value.replace('%','')).toFixed(2)
+    var percent = (percentage / 100.0)
+    var total_percent =  percent + 1
+
+    var costPrice = parseFloat(retailPrice / total_percent).toFixed(2)
+    var costPriceTax = parseFloat(costPrice * 0.1).toFixed(2)
+    var costPriceTotal = (parseFloat(costPrice) + parseFloat(costPriceTax)).toFixed(2)
+    var markUpAmount = parseFloat(costPrice * percent).toFixed(2)
+    var adjustedretailPrice = (parseFloat(costPrice) + parseFloat(markUpAmount)).toFixed(2)
+    var retailPriceTax = parseFloat(adjustedretailPrice * 0.1).toFixed(2)
+    var retailPriceTotal = (parseFloat(adjustedretailPrice) + parseFloat(retailPriceTax)).toFixed(2)
+
+    var totalCostsForSale = 0.0
+    var retailTaxLiability = 0.0
+
+    if (registeredForTax) {
+      totalCostsForSale = retailTaxLiability + parseFloat(costPriceTotal)
+      retailTaxLiability = retailPriceTax - costPriceTax
+    } else {
+      totalCostsForSale = retailTaxLiability + parseFloat(costPrice)
+      retailTaxLiability = retailPriceTax
+    }
+
+    var profit = retailPriceTotal - totalCostsForSale
+
+    this.retailCalculatedRetailPriceTarget.innerText = "$" + adjustedretailPrice
+    if (registeredForTax) {
+      this.retailCalculatedCostPriceTaxTarget.innerText = "$" + costPriceTax
+      this.retailCalculatedCostPriceTotalTarget.innerText = "$" + costPriceTotal
+    } else {
+      this.retailCalculatedRetailPriceTaxTarget.innerText = "n/a"
+      this.retailCalculatedRetailPriceTotalTarget.innerText = "n/a"
+    }
+    this.retailCalculatedCostPriceTarget.innerText = "$" + costPrice
+    this.retailCalculatedMarkupAmountTarget.innerText = "$" + markUpAmount
+    this.retailCalculatedRetailPriceTaxTarget.innerText = "$" + retailPriceTax
+    this.retailCalculatedRetailPriceTotalTarget.innerText = "$" + retailPriceTotal
+    this.retailCalculatedRetailTaxLiabilityTarget.innerText = "$" + parseFloat(retailTaxLiability).toFixed(2)
+    this.retailCalculatedProfitAmountTarget.innerText = "$" + parseFloat(profit).toFixed(2)
   }
 
   calculateAmountsForGivenCostPrice(registeredForTax) {
@@ -88,11 +143,7 @@ export default class extends Controller {
       this.calculatedCostPriceTaxTarget.innerText = "n/a"
       this.calculatedCostPriceTotalTarget.innerText = "n/a"
     }
-    // this.calculatedCostPriceTaxTarget.innerText = "$" + costPriceTax
-    // this.calculatedCostPriceTotalTarget.innerText = "$" + costPriceTotal
-
     this.calculatedMarkupAmountTarget.innerText = "$" + parseFloat(markUpAmount).toFixed(2)
-
     this.calculatedRetailPriceTarget.innerText = "$" + parseFloat(retailPrice).toFixed(2)
     this.calculatedRetailPriceTaxTarget.innerText = "$" + retailPriceTax
     this.calculatedRetailPriceTotalTarget.innerText = "$" + retailPriceTotal
